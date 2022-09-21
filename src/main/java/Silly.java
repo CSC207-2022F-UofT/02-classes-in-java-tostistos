099b9a6a9a3994ec74899ad3757592d49807faa4
tostistos <fidler.patrick@gmail.com>
2022-09-21 17:29:48 -0400

    Implemented TODOs to Silly.java

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -73,13 +73,15 @@ public class Silly implements Comparable<Silly>{
     }
 
     /**
-     * TODO (Task 1): Create another constructor that takes in TWO parameters,
-     *       both of which are strings.
-     *       Afterwards, set this.name to the concatenation of both
-     *       strings (e.g. this.name = [first string] + [second string]).
-     *       Make sure you document this method!
+     * Creates a new Silly object.
+     * This constructor takes two Strings as arguments.
+     *
+     * @param first the first part of this Silly instance's name.
+     * @param second the second part of this Silly instance's name.
      */
-
+    public Silly(String first, String second){
+        this.name = first + second;
+    }
 
 
 
@@ -116,7 +118,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -133,7 +135,7 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +163,9 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        String s1 = other.name;
+        String s2 = this.name;
+        return s2.equals(s1);
     }
 
     /**
@@ -194,6 +199,11 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+
+
+        int other_length = other.name.length();
+        int this_length = this.name.length();
+        return Integer.compare(this_length, other_length);
     }
 
     /*
